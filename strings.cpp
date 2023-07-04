1b656f3ccee69ead78a0d93afc5ecdc5bc804723
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-07-04 22:29:57 +1000

    more

--- strings.cpp
+++ strings.cpp
@@ -1,3 +1,5 @@
+// COMPLETELY UPDATED FOR 50 Char
+
 #include "stdafx.h"
 #include "strings.h"
 
@@ -70,37 +72,34 @@ const tofmsgs &knife_melee()
             "A quick stroke, but the " D " is\non guard.",
             "A good stroke, but it's too slow, the " D "\ndodges."
         },
-
-//!! UP TO HERE!! 
-
         {
             "The haft of your knife knocks out the " D ".",
             "The " D " drops to the floor, unconscious.",
             "The " D " is knocked out!",
         },
         {
-            "The end for the " D " as your knife severs his jugular.",
-            "The fatal thrust strikes the " D " square in the heart:  He dies.",
-            "The " D " takes a final blow and slumps to the floor dead."
+            "The end for the " D " as your knife severs\nhis jugular.",
+            "The fatal thrust strikes the " D " square\nin the heart:  He dies.",
+            "The " D " takes a final blow and slumps\nto the floor dead."
         },
         {
-            "The " D " is slashed on the arm, blood begins to trickle down.",
-            "Your knife point pinks the " D " on the wrist, but it's not serious.",
-            "Your stroke lands, but it was only the flat of the blade.",
-            "The blow lands, making a shallow gash in the " D "'s arm!"
+            "The " D " is slashed on the arm, blood\nbegins to trickle down.",
+            "Your knife point pinks the " D " on the\nwrist, but it's not serious.",
+            "Your stroke lands, but it was only the flat of the\nblade.",
+            "The blow lands, making a shallow gash in the\n" D "'s arm!"
         },
         {
             "The " D " receives a deep gash in his side.",
-            "A savage cut on the leg stuns the " D ", but he can still fight!",
-            "Slash!  Your stroke connects!  The " D " could be in serious trouble!"
+            "A savage cut on the leg stuns the " D ",\nbut he can still fight!",
+            "Slash!  Your stroke connects!  The " D "\ncould be in serious trouble!"
         },
         {
             "The " D " drops to his knees, staggered.",
             "The " D " is confused and can't fight back.",
-            "The quickness of your thrust knocks the " D " back, stunned."
+            "The quickness of your thrust knocks the\n" D " back, stunned."
         },
         {
-            "The " D " is disarmed by a subtle feint past his guard."
+            "The " D " is disarmed by a subtle feint\npast his guard."
         }
     };
     return km;
@@ -112,39 +111,39 @@ const tofmsgs &sword_melee()
     {
         {
             "Your swing misses the " D " by an inch.",
-            "A mighty blow, but it misses the " D " by a mile.",
-            "You charge, but the " D " jumps nimbly aside.",
+            "A mighty blow, but it misses the " D "\nby a mile.",
+            "You charge, but the " D " jumps nimbly\naside.",
             "Clang! Crash! The " D " parries.",
-            "A good stroke, but it's too slow, the " D " dodges."
+            "A good stroke, but it's too slow, the " D "\ndodges."
         },
         {
-            "Your sword crashes down, knocking the " D " into dreamland.",
+            "Your sword crashes down, knocking the\n" D " into dreamland.",
             "The " D " is battered into unconsciousness.",
-            "A furious exchange, and the " D " is knocked out!"
+            "A furious exchange, and the " D " is\nknocked out!"
         },
         {
-            "It's curtains for the " D " as your sword removes his head.",
-            "The fatal blow strikes the " D " square in the heart:  He dies.",
-            "The " D " takes a final blow and slumps to the floor dead."
+            "It's curtains for the " D " as your sword\nremoves his head.",
+            "The fatal blow strikes the " D " square\nin the heart:  He dies.",
+            "The " D " takes a final blow and slumps\nto the floor dead."
         },
         {
-            "The " D " is struck on the arm, blood begins to trickle down.",
-            "Your sword pinks the " D " on the wrist, but it's not serious.",
-            "Your stroke lands, but it was only the flat of the blade.",
-            "The blow lands, making a shallow gash in the " D "'s arm!"
+            "The " D " is struck on the arm, blood\nbegins to trickle down.",
+            "Your sword pinks the " D " on the wrist,\nbut it's not serious.",
+            "Your stroke lands, but it was only the flat of the\nblade.",
+            "The blow lands, making a shallow gash in the\n" D "'s arm!"
         },
         {
             "The " D " receives a deep gash in his side.",
-            "A savage blow on the thigh!  The " D " is stunned but can still fight!",
-            "Slash!  Your blow lands!  That one hit an artery, it could be serious!"
+            "A savage blow on the thigh!  The " D " is\nstunned but can still fight!",
+            "Slash!  Your blow lands!  That one hit an artery,\nit could be serious!"
         },
         {
-            "The " D " is staggered, and drops to his knees.",
-            "The " D " is momentarily disoriented and can't fight back.",
-            "The force of your blow knocks the " D " back, stunned."
+            "The " D " is staggered, and drops to his\nknees.",
+            "The " D " is momentarily disoriented and\ncan't fight back.",
+            "The force of your blow knocks the " D "\nback, stunned."
         },
         {
-            "The " D "'s weapon is knocked to the floor, leaving him unarmed."
+            "The " D "'s weapon is knocked to the floor,\nleaving him unarmed."
         }
     };
     return sm;
@@ -155,58 +154,54 @@ const tofmsgs &thief_melee()
     static const tofmsgs tm =
     {
         {
-            "The thief stabs nonchalantly with his stiletto and misses.",
+            "The thief stabs nonchalantly with his stiletto\nand misses.",
             "You dodge as the thief comes in low.",
-            "You parry a lightning thrust, and the thief salutes you with\n"
-                "a grim nod.",
-            "The thief tries to sneak past your guard, but you twist away."
+            "You parry a lightning thrust, and the thief\nsalutes you with a grim nod.",
+            "The thief tries to sneak past your guard, but you\ntwist away."
         },
         {
-            "Shifting in the midst of a thrust, the thief knocks you unconscious\n"
-                "with the haft of his stiletto.",
+            "Shifting in the midst of a thrust, the thief knocks\nyou unconscious with the haft of his stiletto.",
             "The thief knocks you out.",
         },
         {
-            "Finishing you off, a lightning throw right to the heart.",
-            "The stiletto severs your jugular.  It looks like the end.",
-            "The thief comes in from the side, feints, and inserts the blade\n"
-                "into your ribs.",
-            "The thief bows formally, raises his stiletto, and with a wry grin,\n"
-                "ends the battle and your life."
+            "Finishing you off, a lightning throw right to the\nheart.",
+            "The stiletto severs your jugular.  It looks like\nthe end.",
+            "The thief comes in from the side, feints, and\ninserts the blade into your ribs.",
+            "The thief bows formally, raises his stiletto, and\nwith a wry grin, ends the battle and your life."
         },
         {
-            "A quick thrust pinks your left arm, and blood starts to\ntrickle down.",
-            "The thief draws blood, raking his stiletto across your arm.",
-            "The stiletto flashes faster than you can follow, and blood wells\nfrom your leg.",
-            "The thief slowly approaches, strikes like a snake, and leaves\nyou wounded."
+            "A quick thrust pinks your left arm, and blood\nstarts to trickle down.",
+            "The thief draws blood, raking his stiletto across\nyour arm.",
+            "The stiletto flashes faster than you can follow,\nand blood wells from your leg.",
+            "The thief slowly approaches, strikes like a snake,\nand leaves you wounded."
         },
         {
-            "The thief strikes like a snake!  The resulting wound is serious.",
+            "The thief strikes like a snake!  The resulting\nwound is serious.",
             "The thief stabs a deep cut in your upper arm.",
-            "The stiletto touches your forehead, and the blood obscures your\nvision.",
-            "The thief strikes at your wrist, and suddenly your grip is slippery\nwith blood."
+            "The stiletto touches your forehead, and the blood\nobscures your vision.",
+            "The thief strikes at your wrist, and suddenly your\ngrip is slippery with blood."
         },
         {
-            "The butt of his stiletto cracks you on the skull, and you stagger\nback.",
-            "You are forced back, and trip over your own feet, falling heavily\nto the floor.",
-            "The thief rams the haft of his blade into your stomach, leaving\nyou out of breath.",
+            "The butt of his stiletto cracks you on the skull,\nand you stagger back.",
+            "You are forced back, and trip over your own feet,\nfalling heavily to the floor.",
+            "The thief rams the haft of his blade into your\nstomach, leaving you out of breath.",
             "The thief attacks, and you fall back desperately."
         },
         {
-            "A long, theatrical slash.  You catch it on your " W ", but the\nthief twists his knife, and the " W " goes flying.",
-            "The thief neatly flips your " W " out of your hands, and it drops\nto the floor.",
-            "You parry a low thrust, and your " W " slips out of your hand.",
-            "Avoiding the thief's stiletto, you stumble to the floor, dropping\nyour " W "."
+            "A long, theatrical slash.  You catch it on your\n" W ", but the thief twists his knife, and\nthe " W " goes flying.",
+            "The thief neatly flips your " W " out of your\nhands, and it drops to the floor.",
+            "You parry a low thrust, and your " W " slips\nout of your hand.",
+            "Avoiding the thief's stiletto, you stumble to the\nfloor, dropping your " W "."
         },
         {
-            "The thief, a man of good breeding, refrains from attacking a helpless\nopponent.",
-            "The thief amuses himself by searching your pockets.",
+            "The thief, a man of good breeding, refrains from\nattacking a helpless opponent.",
+            "The thief amuses himself by searching your\npockets.",
             "The thief entertains himself by rifling your pack.",
         },
         {
-            "The thief, noticing you begin to stir, reluctantly finishes you off.",
-            "The thief, forgetting his essentially genteel upbringing, cuts your\nthroat.",
-            "The thief, who is essentially a pragmatist, dispatches you as a\nthreat to his livelihood."
+            "The thief, noticing you begin to stir, reluctantly\nfinishes you off.",
+            "The thief, forgetting his essentially genteel\nupbringing, cuts your throat.",
+            "The thief, who is essentially a pragmatist,\ndispatches you as a threat to his livelihood."
         }
     };
     return tm;
@@ -223,7 +218,7 @@ const tofmsgs &troll_melee()
             "The axe crashes against the rock, throwing sparks!"
         },
         {
-            "The flat of the troll's axe hits you delicately on the head, knocking\nyou out."
+            "The flat of the troll's axe hits you delicately\non the head, knocking you out."
         },
         {
             "The troll lands a killing blow.  You are dead.",
@@ -233,33 +228,33 @@ const tofmsgs &troll_melee()
         },
         {
             "The axe gets you right in the side.  Ouch!",
-            "The flat of the troll's axe skins across your forearm.",
-            "The troll's swing almost knocks you over as you barely parry\nin time.",
-            "The troll swings his axe, and it nicks your arm as you dodge."
+            "The flat of the troll's axe skins across your\nforearm.",
+            "The troll's swing almost knocks you over as you\nbarely parry in time.",
+            "The troll swings his axe, and it nicks your arm\nas you dodge."
         },
         {
-            "The troll charges, and his axe slashes you on your " W " arm.",
+            "The troll charges, and his axe slashes you on your\n" W " arm.",
             "An axe stroke makes a deep wound in your leg.",
-            "The troll's axe swings down, gashing your shoulder.",
-            "The troll sees a hole in your defense, and a lightning stroke\nopens a wound in your left side."
+            "The troll's axe swings down, gashing your\nshoulder.",
+            "The troll sees a hole in your defense, and a\nlightning stroke opens a wound in your left side."
         },
         {
-            "The troll hits you with a glancing blow, and you are momentarily\nstunned.",
-            "The troll swings; the blade turns on your armor but crashes\nbroadside into your head.",
+            "The troll hits you with a glancing blow, and you\nare momentarily stunned.",
+            "The troll swings; the blade turns on your armor\nbut crashes broadside into your head.",
             "You stagger back under a hail of axe strokes.",
             "The troll's mighty blow drops you to your knees."
         },
         {
             "The axe hits your " W " and knocks it spinning.",
-            "The troll swings, you parry, but the force of his blow disarms you.",
-            "The axe knocks your " W " out of your hand.  It falls to the floor.",
-            "Your " W " is knocked out of your hands, but you parried the blow."
+            "The troll swings, you parry, but the force of his\nblow disarms you.",
+            "The axe knocks your " W " out of your hand.  It\nfalls to the floor.",
+            "Your " W " is knocked out of your hands, but you\nparried the blow."
         },
         {
-            "The troll strikes at your unconscious form, but misses in his rage.",
+            "The troll strikes at your unconscious form, but\nmisses in his rage.",
             "The troll hesitates, fingering his axe.",
-            "The troll scratches his head ruminatively:  Might you be magically\nprotected, he wonders? ",
-            "The troll seems afraid to approach your crumpled form."
+            "The troll scratches his head ruminatively:  Might\nyou be magically protected, he wonders? ",
+            "The troll seems afraid to approach your crumpled\nform."
         },
         {
             "Conquering his fears, the troll puts you to death."
